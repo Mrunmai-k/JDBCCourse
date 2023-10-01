e956516e737dd632fcb02a8dc3b20892124f86df
mrunmai <mrunmaikudale18@gmail.com>
2023-10-01 23:27:13 +0530

    Update query execution of database

--- src/CRUD.java
+++ src/CRUD.java
@@ -4,13 +4,13 @@ public class CRUD {
         String url = "jdbc:postgresql://localhost:5432/demo" ;
         String user = "postgres";
         String pass = "Mrunmai@24";
-        String ins = "insert into student values(3,'Bhagyashri',56)";
-        //String up = "update student set sname='John' where sname='Bhagyashri'";
+        //String ins = "insert into student values(3,'Bhagyashri',56)";
+        String up = "update student set sname='John' where sname='Bhagyashri'";
 
         Connection conn = DriverManager.getConnection(url,user,pass);  //SQLException
         System.out.println("Connection Established");
         Statement st = conn.createStatement();
-        System.out.println(st.execute(ins));
+        System.out.println(st.execute(up));
 
 
         conn.close();
