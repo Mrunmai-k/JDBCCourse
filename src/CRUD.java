a6823afe01821523707b81e560b0a56831e8b337
mrunmai <mrunmaikudale18@gmail.com>
2023-10-01 23:28:20 +0530

    Delete query execution of database

--- src/CRUD.java
+++ src/CRUD.java
@@ -4,13 +4,14 @@ public class CRUD {
         String url = "jdbc:postgresql://localhost:5432/demo" ;
         String user = "postgres";
         String pass = "Mrunmai@24";
-        //String ins = "insert into student values(3,'Bhagyashri',56)";
-        String up = "update student set sname='John' where sname='Bhagyashri'";
 
+        //String ins = "insert into student values(3,'Bhagyashri',56)";
+        //String up = "update student set sname='John' where sname='Bhagyashri'";
+        String del = "delete from student where sid = 3";
         Connection conn = DriverManager.getConnection(url,user,pass);  //SQLException
         System.out.println("Connection Established");
         Statement st = conn.createStatement();
-        System.out.println(st.execute(up));
+        System.out.println(st.execute(del));
 
 
         conn.close();
