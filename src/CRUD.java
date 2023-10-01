bf4f8e14a3e011c03362e3ada22883619630e024
mrunmai <mrunmaikudale18@gmail.com>
2023-10-02 00:26:20 +0530

    Prepared Statement usage and execution of database

--- src/CRUD.java
+++ src/CRUD.java
@@ -6,21 +6,34 @@ public class CRUD {
         String user = "postgres";
         String pass = "Mrunmai@24";
 
-        int sid = 5;
-        String name = "Momo";
-        int marks = 67;
+        int sid = 4;
+        String name = "Mitali";
+        int marks = 55;
+        /*
 
         //String ins1 = "insert into student values(3,'Bhagyashri',56)";
-        String ins2 = "insert into student values(" + sid + ",'" + name + "'," + marks + ")";
+        //String ins2 = "insert into student values(" + sid + ",'" + name + "'," + marks + ")";
         //String up = "update student set sname='John' where sname='Bhagyashri'";
         //String del = "delete from student where sid = 3";
+         */
+
         Connection conn = DriverManager.getConnection(url, user, pass);  //SQLException
         System.out.println("Connection Established");
-        Statement st = conn.createStatement();
-        System.out.println(st.execute(ins2));
+//        Statement st = conn.createStatement();
+//        System.out.println(st.execute(ins2));
 
+        String sql = "insert into student values(?,?,?)";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1,sid);
+        ps.setString(2,name);
+        ps.setInt(3,marks);
+
+        ps.execute();
 
         conn.close();
         System.out.println("Connection Closed");
+
+
+
     }
 }
